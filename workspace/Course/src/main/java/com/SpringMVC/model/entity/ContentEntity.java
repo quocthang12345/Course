68e78e028ccb1c5f9eee1a13108479e1adfccb0b
Quoc Thang <quocthang1100@gmail.com>
2020-08-29 01:36:12 +0700

    Day 6 create Controller and Model for Project

---
+++ workspace/Course/src/main/java/com/SpringMVC/model/entity/ContentEntity.java
@@ -0,0 +1,11 @@
+package com.SpringMVC.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "content-lesson")
+public class ContentEntity extends AbstractEntity {
+	
+	
+}
